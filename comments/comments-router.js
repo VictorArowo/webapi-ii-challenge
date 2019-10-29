3a67a78026dc37474b7ebbc828bf2b873234202f
Victor Arowo <arowovici@gmail.com>
2019-10-29 15:39:46 +0100

    Implemented Redis Caching

--- comments/comments-router.js
+++ comments/comments-router.js
@@ -1,24 +1,45 @@
 const express = require('express');
 const db = require('../data/db');
+const redis = require('redis');
+
+const client = redis.createClient(6379);
 
 const router = express.Router();
 
+client.on('error', err => {
+  console.log('Error ' + err);
+});
+
+client.on('connect', function() {
+  console.log('Redis client connected');
+});
+
 router.get('/:id/comments', (req, res) => {
   const { id } = req.params;
-  db.findPostComments(id)
-    .then(data => {
-      console.log(data);
-      return data.length
-        ? res.status(200).json(data)
-        : res.status(404).json({
-            message: 'The post with the specified ID does not exist.'
+
+  const postRedisKey = 'posts';
+
+  return client.get(postRedisKey, (err, posts) => {
+    if (posts) {
+      return res.status(200).json({ source: 'cache', data: JSON.parse(posts) });
+    } else {
+      db.findPostComments(id)
+        .then(data => {
+          client.setex(postRedisKey, 10, JSON.stringify(data));
+
+          return data.length
+            ? res.status(200).json({ ...data, source: 'api' })
+            : res.status(404).json({
+                message: 'The post with the specified ID does not exist.'
+              });
+        })
+        .catch(() => {
+          return res.status(500).json({
+            error: 'The comments information could not be retrieved.'
           });
-    })
-    .catch(() => {
-      return res.status(500).json({
-        error: 'The comments information could not be retrieved.'
-      });
-    });
+        });
+    }
+  });
 });
 
 router.post('/:id/comments', (req, res) => {
